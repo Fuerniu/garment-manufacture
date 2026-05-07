6b41df12372d1773d17734dda6835f83a35fd88f
fuerniu <1774692335@qq.com>
2026-05-08 00:52:52 +0800

    Fix: thanks page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     // ... 其他配置
-    trailingSlash: 'always',     // 推荐带斜杠
+    // trailingSlash: 'always',     // 推荐带斜杠
 });
