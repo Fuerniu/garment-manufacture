f0068d64d78c4935bd328b6902abf6190a20cf84
fuerniu <1774692335@qq.com>
2026-05-07 23:47:20 +0800

    Fix: thank-you page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // ... 其他配置
+    trailingSlash: 'always',     // 推荐带斜杠
+    build: {
+        format: 'directory',       // 生成 /thank-you/index.html
+    }
+});
